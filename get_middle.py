f86bbd020f30692773b0e3197811df2f4440bd8a
Erick Bezerra <erick.bezerra@gmail.com>
2022-09-13 09:44:21 -0300

    def descripton change

--- get_middle.py
+++ get_middle.py
@@ -2,9 +2,9 @@ def get_middle(s):
     """Get the Middle Character
 
     Args:
-        s (str): description'
+        s (str): any string
 
     Returns:
-        get_middle(s): Coupole of strings (pair) and doubled middle char (odd)         
+        get_middle(s): Couple of strings (pair) and doubled middle char (odd)         
     """    
     return s[(len(s)-1)//2:(len(s)+2)//2]
